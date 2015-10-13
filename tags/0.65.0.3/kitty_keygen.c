cef165b1bdaf347d5924b26b0858391335983568
guest <guest@fc775d32-b62a-484d-ad2e-5dc0bf04650a>
2015-10-13 12:32:10 +0000

    Error --Thir line, and those below, will be ignored-- r
    D    svn://localhost/0.65.0.3
    
    
    git-svn-id: http://svn.9bis.com/sandbox/kitty@74 fc775d32-b62a-484d-ad2e-5dc0bf04650a

--- tags/0.65.0.3/kitty_keygen.c
+++
@@ -1,2 +0,0 @@
-int get_param( const char * val ) { return 0 ; }
-void debug_logevent( const char *fmt, ... ) { return ; }
--- tags/0.65.0.3/wincrypt/wincapi.h
+++
@@ -1,3 +0,0 @@
-void* capi_rsa2_sign(void *pRSAKeyStruct, char *data, int datalen);
-int capi_is_capikey(void *pRSAKeyStruct);
-void *capi_load_key(unsigned char **blob, int *len);
